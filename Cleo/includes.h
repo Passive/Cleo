02da1ea04d6f9c45c227b44c98692b7880cad9a1
Passive <40716556+Passive@users.noreply.github.com>
2022-01-24 15:03:34 +1300

    Create includes.h

---
+++ Cleo/includes.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <regex>
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <Windows.h>
+#include <sys/stat.h>
+#include <thread>
+#include <wininet.h>
+#include <intrin.h>
+#include <stdarg.h>
+#include <cpr/cpr.h>
+#include <json/json.h>
+//
+#include "XorStr.h"
+#include "structs.h"
+#include "config.h"
+#include "discord.h"
+#include "anti.h"
+#include "modules.h"
+
+
+
