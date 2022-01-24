bf96cc0c34df3835f9d6aa493115d7c9b292d8b0
Passive <40716556+Passive@users.noreply.github.com>
2022-01-24 15:03:57 +1300

    Create structs.h

---
+++ Cleo/structs.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "includes.h"
+
+#define MAX_NAME 32
+
+// TODO: implement driver structs
+typedef struct {
+	CHAR local_user[512];
+	CHAR host[124];
+	std::string inet_addr;
+} SystemQueryInformation_t;
