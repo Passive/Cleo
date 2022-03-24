ea355d52a3fc96d0572d6c77e76543d60937bbe4
Passive <40716556+Passive@users.noreply.github.com>
2022-03-24 16:34:51 +1300

    Update structs.h

--- Cleo/structs.h
+++ Cleo/structs.h
@@ -4,7 +4,7 @@
 #define MAX_NAME 32
 
 // TODO: implement driver structs
-typedef struct SystemQueryInformation {
+typedef struct SystemQueryInformation_t {
 	CHAR local_user[512];
 	CHAR host[124];
 	std::string inet_addr;
